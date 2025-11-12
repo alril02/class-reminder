0da53be6119128d826042e818c7e2ebd4518f41c
mogeleaf-del <mogeleaf@gmail.com>
2025-11-12 14:11:05 +0800

    menu Login
    perancangan menu login

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,10 +1,20 @@
 <template>
+<<<<<<< Updated upstream
   <q-layout view="hHh Lpr fFf">
     <!-- Be sure to play with the Layout demo on docs -->
+=======
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+
+>>>>>>> Stashed changes
 
     <q-page-container>
       <!-- This is where pages get injected -->
       <router-view />
     </q-page-container>
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
   </q-layout>
 </template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,23 @@ const routes = [
       { path: '/signup', component: () => import('pages/SignUpPage.vue') },
     ],
   },
+<<<<<<< Updated upstream
+=======
+{
+  path: '/login',
+  componen: () =>import('layouts/LoginLayout.vue'),
+  children: [
+    { path: '', component: () => import('pages/login/LoginPage.vue')}
+  ]
+
+},
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+>>>>>>> Stashed changes
 ]
 
 export default routes
